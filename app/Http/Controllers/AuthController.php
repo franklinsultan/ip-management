8d819c363ea0c8d09b9aea183495d68b16c75142
Franklin Sultan <cdb.franklinsultan@gmail.com>
2024-08-21 11:46:41 +0800

    add user registration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user', 'token'), 201);
+    }
+}
---
+++ resources/js/components/Register.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2>Register</h2>
+        <form @submit.prevent="register">
+            <input v-model="user.name" type="text" placeholder="Name" required />
+            <input v-model="user.email" type="email" placeholder="Email" required />
+            <input v-model="user.password" type="password" placeholder="Password" required />
+            <input v-model="user.password_confirmation" type="password" placeholder="Confirm Password" required />
+            <button type="submit">Register</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { register } from '../auth/index';
+
+export default {
+    data() {
+        return {
+            user: {
+                name: '',
+                email: '',
+                password: '',
+                password_confirmation: ''
+            }
+        };
+    },
+    methods: {
+        register() {
+            register(this.user).then(response => {
+                console.log(response.data);
+            }).catch(error => {
+                console.error(error);
+            });
+        }
+    }
+};
+</script>
